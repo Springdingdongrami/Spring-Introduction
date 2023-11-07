7f6660b559099fc858f57222787d7fa3a625692c
limsubinn <mymyx02@naver.com>
2023-11-07 16:28:33 +0900

    6. 스프링 DB 접근 기술

--- limsubinn/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ limsubinn/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,15 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY -> DB가 id를 자동으로 생성
     private Long id;
     private String name;
 
